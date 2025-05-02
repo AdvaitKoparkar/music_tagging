60515d16349ccbdf48d687f529a05a0f56f9e4fb
Advait Koparkar <advaitkoparkar@gmail.com>
2025-05-01 19:15:59 -0700

    try audio separator preprocessing

--- setup.sh
+++ setup.sh
@@ -4,5 +4,7 @@ python3 -m pip install --upgrade pip
 pip3 install -r requirements.txt
 pip3 install torch torchvision torchaudio
 
+brew update; brew install ffmpeg
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 echo "export PYTHONPATH=$PYTHONPATH:$(pwd)" >> .music_tagging_env/bin/activate
+audio-separator --env_info
